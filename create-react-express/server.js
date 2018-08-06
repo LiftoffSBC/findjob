92136c2406e29a2c4c140a734e94ec711fe5ff53
Missy Norman <missy.norman412@gmail.com>
2018-08-06 13:40:42 -0400

    initial push career one stop search

--- create-react-express/server.js
+++ create-react-express/server.js
@@ -2,6 +2,25 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const request = require("request");
+
+app.get("/api/search/:title", (req, res) => {
+  
+  const options = {
+    url: `https://api.careeronestop.org/v1/jobsearch/q2Tg69QZctxkreR/${req.params.title}/charlotte%2C%20nc/25/0/asc/0/10/30`,
+    headers: {
+      "Authorization": "Bearer eSYovS0Vc2Nrqm7mTFo0ilggJpIwdYpjuysry3Ej/Wcxbykv3Bp6aG+AZE4VpOQad5fmn7t++cH6N4qrixfh2w=="
+    }
+  }
+  request(options, (err, response, body) => {
+    if (!err && response.statusCode === 200) {
+      res.json(body);
+      console.log(body);
+    } else {
+      res.json({ error: err.message });
+    }
+  });
+});
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -17,3 +36,7 @@ app.get("*", function(req, res) {
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
+
+
+
+
