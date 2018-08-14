b76549b3a7bfb6ac67a07f424d11ea114eb54341
Austin Dailly <austin@wifi_stu-10-38-64-60.uncc.edu>
2018-08-14 14:45:04 -0400

    Added table comp

---
+++ findjob/src/components/Table/Table.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Table.css";
+
+const Table = () => (
+    <div className="table">
+                <thead id="thead">
+                  <tr>
+                    <th>#</th>
+                    <th>Job</th>
+                    <th>Location</th>
+                    <th>Pay</th>
+                  </tr>
+                </thead>
+                <tbody id="tbody">
+                  <tr>
+                    <td>1</td>
+                    <td>Full-Stack Developer</td>
+                    <td>Charlotte, NC</td>
+                    <td>$85k</td>
+                  </tr>
+                </tbody>
+        </div>
+  );
+
+export default Table;
