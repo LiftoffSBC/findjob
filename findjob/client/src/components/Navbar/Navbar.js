8e42f58ca8c4a6bc53f3609be2f54e5bb210a67b
Danielson Dailly <danyellow703@gmail.com>
2018-08-14 23:13:56 -0400

    new

--- findjob/client/src/components/Navbar/Navbar.js
+++ findjob/client/src/components/Navbar/Navbar.js
@@ -6,7 +6,10 @@ const Navbar = () => (
   <a className="navbar-brand"><img src={require('../../img/finjobiotransparent.png')} /></a>
   <form className="form-inline">
     <button className="btn btn-outline-success my-2 my-md-0" type="submit" onClick={this.loadCareer}>Search</button>
-    <input className="form-control mr-lg-2" type="search" placeholder="Search" aria-label="Search" controlId="formHorizontalName" />
+    <input className="form-control mr-lg-2" type="search" placeholder="City" aria-label="city" controlId="formHorizontalName" />
+    <input className="form-control mr-lg-2" type="search" placeholder="State" aria-label="state" controlId="formHorizontalName" />
+    <input className="form-control mr-lg-2" type="search" placeholder="Zip" aria-label="Zip" controlId="formHorizontalName" />
+
   </form>
 </nav>
 );
